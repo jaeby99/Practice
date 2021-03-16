9df23ffc87349a9406ecd82d2fb560da0f233051
- <qo0102@naver.com>
2021-03-16 13:24:56 +0000

    smart way of join

--- algorithm_py/strangeWord.py
+++ algorithm_py/strangeWord.py
@@ -1,16 +1,2 @@
 def solution(s):
-    d=0
-    answer=""
-    words=list(s)
-    for x in range(len(words)):
-        w="".join(words[x])
-        if words[x]==" ":
-            d=0
-        elif d==0:
-            w=w.upper()
-            d=1
-        elif d==1:
-            w=w.lower()
-            d=0
-        answer+=w
-    return answer
+    return " ".join(["".join(c.upper() if i%2==0 else c.lower() for i,c in enumerate(w))for w in s.split(" ")])
