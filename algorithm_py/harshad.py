f1af9aed181e1721ab50810710725d710c517866
- <qo0102@naver.com>
2021-03-22 14:19:24 +0000

    learned

--- algorithm_py/harshad.py
+++ algorithm_py/harshad.py
@@ -1,7 +1,2 @@
 def solution(x):
-    num=x
-    div=0
-    for i in range(len(str(x))-1,-1,-1):
-        div=div+int(num/10**i)
-        num=num%10**i
-    return x%div==0
+    return x%sum(int(n) for n in str(x))==0
