03132382859bc24cf44c8750a8409ece523a84e1
- <qo0102@naver.com>
2021-04-22 14:04:48 +0000

    study heap

---
+++ algorithm2_py/spicy.py
@@ -0,0 +1,15 @@
+import heapq
+
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    answer=0
+    
+    while True:
+        first=heapq.heappop(scoville)
+        if first>=K: break
+        if len(scoville)==0: return -1
+        second=heapq.heappop(scoville)
+        heapq.heappush(scoville, first+2*second)
+        answer+=1
+    
+    return answer
