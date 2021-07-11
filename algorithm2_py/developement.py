a4bfc9e9c6d5c94ab383f5cd92975c5e4a39956a
- <qo0102@naver.com>
2021-07-11 11:16:31 +0000

    Stack/Queue 1

--- algorithm2_py/developement.py
+++ algorithm2_py/developement.py
@@ -1,18 +1,8 @@
-import math
-
 def solution(progresses, speeds):
-    days=[]
-    answer=[]
-    for x in range(len(speeds)):
-        day=math.ceil((100-progresses[x])/speeds[x])
-        days.append(day)
-    distribute=days[0]
-    n=1
-    for x in range(1,len(days)):
-        if distribute<days[x]:
-            answer.append(n)
-            distribute=days[x]
-            n=1
-        else: n+=1
-    answer.append(n)
-    return answer
+    d=[]
+    for p,s in zip(progresses, speeds):
+        if len(d)==0 or d[-1][0] < -((p-100)//s):
+            d.append([-((p-100)//s),1])
+        else:
+            d[-1][1]+=1
+    return [x[1] for x in d]
