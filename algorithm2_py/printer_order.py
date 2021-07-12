e7f59a9de78721610d250a0c19ff07f076c7fe3b
- <qo0102@naver.com>
2021-07-12 04:57:20 +0000

    Stack/Queue 2

--- algorithm2_py/printer_order.py
+++ algorithm2_py/printer_order.py
@@ -1,14 +1,22 @@
+from collections import deque
+
 def solution(priorities, location):
-    sorted_priorities=sorted(priorities, reverse=True)
     count=0
-    pos=0
-    for x in sorted_priorities:
-        for n in range(len(priorities)):
-            if pos>=len(priorities): pos=0
-            if x==priorities[pos]:
-                count+=1
-                if pos==location: return count
-                else: pos+=1
-                break
-            else: pos+=1
-        if pos>=len(priorities): pos=0
+    length=len(priorities)
+    enum_prior=deque(enumerate(priorities))
+    p=max(priorities)
+    
+    while len(enum_prior)!=0:
+        if enum_prior[0][1]!=p:
+            key=enum_prior[0]
+            enum_prior.popleft()
+            enum_prior.append(key)
+        else:
+            count+=1
+            if enum_prior[0][0]==location:
+                return count
+            else:
+                priorities.remove(p)
+                enum_prior.popleft()
+                p=max(priorities)
+    return length
